a4dae2f7aa931ceb092487ef879e06430774c8a4
junior.raveline@gmail.com <junior.raveline@gmail.com>
2021-04-22 20:46:44 -0300

    SignUpActivity.kt created to register new user

--- app/src/main/java/br/com/trelloapp/ui/SignUpActivity.kt
+++ app/src/main/java/br/com/trelloapp/ui/SignUpActivity.kt
@@ -1,21 +1,35 @@
 package br.com.trelloapp.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.view.View
 import android.view.WindowManager
+import android.widget.Toast
 import br.com.trelloapp.R
+import br.com.trelloapp.utils.Constants.isNetworkAvailable
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_sign_up.*
 
-class SignUpActivity : AppCompatActivity() {
+class SignUpActivity : BaseActivity(), View.OnClickListener {
+
+    private lateinit var firebaseInstance: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+        initFirebaseInstance()
 
         setupActionBar()
 
+        btn_sign_up.setOnClickListener(this)
+
     }
 
-    private fun setupActionBar(){
+    private fun initFirebaseInstance() {
+        firebaseInstance = FirebaseAuth.getInstance()
+    }
+
+    private fun setupActionBar() {
 
         window.setFlags(
             WindowManager.LayoutParams.FLAG_FULLSCREEN,
@@ -25,9 +39,70 @@ class SignUpActivity : AppCompatActivity() {
         setSupportActionBar(toolbar_sign_up_activity)
 
         val actionBar = supportActionBar
-        if(actionBar != null){
+        if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true)
             actionBar.setHomeAsUpIndicator(R.drawable.ic_black_color_back_24dp)
         }
+    }
+
+    private fun registerUser() {
+        val name: String = et_signup_name.text.toString().trim { it <= ' ' }
+        val email: String = et_signup_email.text.toString().trim { it <= ' ' }
+        val password: String = et_signup_password.text.toString().trim { it <= ' ' }
+
+        if (isNetworkAvailable(applicationContext)) {
+            if (validadeForm(name, email, password)) {
+                showProgressDialog(resources.getString(R.string.please_wait))
+                firebaseInstance
+                    .createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener { task ->
+                        hideProgressDialog()
+                        if (task.isSuccessful) {
+                            Toast.makeText(
+                                this,
+                                "$name, Welcome to OrganizeIt´s",
+                                Toast.LENGTH_SHORT
+                            ).show()
+
+                            finish()
+                        } else {
+                            showErrorSnackBar("Something went wrong during your signup! ${task.exception}")
+                        }
+                    }.addOnFailureListener {
+                        exception ->  
+                    }
+            }
+        } else showErrorSnackBar("No Internet Connection")
+
+
+    }
+
+    private fun validadeForm(name: String, email: String, password: String): Boolean {
+        return when {
+            TextUtils.isEmpty(name) -> {
+                showErrorSnackBar("Please set a name")
+                false
+            }
+            TextUtils.isEmpty(email) -> {
+                showErrorSnackBar("Please set an email")
+                false
+            }
+            TextUtils.isEmpty(password) -> {
+                showErrorSnackBar("Please set a password")
+                false
+            }
+
+
+            else -> true
+        }
+    }
+
+    override fun onClick(view: View?) {
+        when (view?.id) {
+            R.id.btn_sign_up -> {
+                registerUser()
+            }
+        }
+
     }
 }
